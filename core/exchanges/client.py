a1d0feae94577269b3f7be7c9b7eec3e43338449
Abrahan <Scr44gr@gmail.com>
2021-05-19 19:05:41 -0400

    Added missing methods and Strategy class

--- core/exchanges/client.py
+++ core/exchanges/client.py
@@ -1,12 +1,17 @@
-from typing import AnyStr
+from typing import AnyStr, List
 from core.exchanges.binance import BinanceClient
+from core.exchanges.strategy import Strategy
 
 
-class Client:
+class Client(Strategy):
 
     def __init__(self, public_key: AnyStr, secret_key: AnyStr, use_testnet=False) -> None:
+        super().__init__()
 
-        self.client = BinanceClient(public_key, secret_key, use_testnet)
+        if (public_key and secret_key) is not None:
+            self.client = BinanceClient(public_key, secret_key, use_testnet)
+        else:
+            raise Exception('API KEYS ERROR, PLEASE CHECK AND RUN THE CLIENT AGAIN!')
 
     def create_buy_order(self, **kwargs):
         """
@@ -63,3 +68,48 @@ class Client:
 
         """
         return self.client.create_sell_order(test_order=True, **kwargs)
+
+    def get_bids_price(self, symbol: AnyStr) -> float:
+        """
+        GET THE ACTUAL BIDS PRICE.
+
+
+        :params:
+
+        - symbol <str>
+        """
+
+        self._prev_bids_price = self.client.get_bid_price(symbol)
+        return self._prev_bids_price
+
+    def get_asks_price(self, symbol: AnyStr) -> float:
+        """
+        GET THE ACTUAL ASKS PRICE.
+
+
+        :params:
+
+        - symbol <str>
+        """
+
+        self._prev_asks_price = self.client.get_ask_price(symbol)
+        return self._prev_asks_price
+
+    def get_open_orders(self) -> List:
+        """
+        GET A LIST OF OPEN ORDERS.
+        """
+
+        return self.client.get_open_orders()
+
+    def cancel_order(self, symbol, order_id):
+        """
+        CANCELS AN OPEN ORDER.
+        
+        :params:
+
+        - symbol <str>
+        - order_id <str>
+
+        """
+        return self.client.cancel_order(symbol, order_id)
