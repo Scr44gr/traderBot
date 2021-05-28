0e7207dd1a58764b9a82f6885eed0d0dc9720dcc
Abrahan <scr44gr@gmail.com>
2021-05-28 18:30:19 -0400

    Update Strategy class

--- core/exchanges/strategy.py
+++ core/exchanges/strategy.py
@@ -1,6 +1,6 @@
 from time import sleep
 from logging import getLogger
-
+from os import getenv
 
 class Strategy:
 
@@ -17,10 +17,20 @@ class Strategy:
     def run(self) -> None:
         self.__logger.info("STRATEGY STARTED")
         while self.process:
+            sleep(int(getenv('STRATEGY_PROCESS_SLEEP_TIME')))
+
             try:
                 self.strategy()
-                sleep(1)
             except:
                 self.__logger.error(
                     "An error has ocurred on self.strategy func, details: ", exc_info=1)
-                break
+                if int(getenv('BREAK_STRATEGY_PROCESS', 0)):
+                    break
+
+    @property
+    def name(self, value):
+        self._name = value
+
+    @name.setter
+    def name(self):
+        return self._name
